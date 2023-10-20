d28ce34326894dff1bba97f2e3b9d2b0caa6d75b
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-20 21:32:49 +0530

    roboshop project - Terraform

--- 06-module-1/main.tf
+++ 06-module-1/main.tf
@@ -6,3 +6,10 @@ module "sample2" {
   source = "./ec2"
   name = "Sample2"
 }
+
+output "public_ip_sample1" {
+  value = module.sample1.public_ip
+}
+output "public_ip_sample2" {
+  value = module.sample2.public_ip
+}
