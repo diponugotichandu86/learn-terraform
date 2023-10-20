f86b0b27d06406805257a393c6bb3d252465b3ee
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-20 20:59:09 +0530

    roboshop project - Terraform

--- 04-resource/main.tf
+++ 04-resource/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "example" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
-
   tags = {
     Name = "HelloWorld"
-  }}
+  }
+}
