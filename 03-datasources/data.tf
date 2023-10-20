718aa5fd6a7272a77bd25b252f37ea7097ab8841
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-20 18:36:13 +0530

    roboshop project - Terraform

--- 03-datasources/data.tf
+++ 03-datasources/data.tf
@@ -9,5 +9,5 @@ data "aws_ec2_spot_price" "example" {
 }
 
 output "price" {
-  value = data.aws_ec2_spot_price.example.id - data.aws_ec2_spot_price.example.spot_price
+  value = data.aws_ec2_spot_price.example.id
 }
