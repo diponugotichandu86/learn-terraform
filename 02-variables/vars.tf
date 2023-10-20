972a8e9e938ffe11d29b397e4fd00ac39a842542
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-20 17:41:23 +0530

    roboshop project - Terraform

--- 02-variables/vars.tf
+++ 02-variables/vars.tf
@@ -17,3 +17,34 @@ output "sampleoutput2" {
 output "sampleoutput3" {
   value = "Value of Sample1 is ${var.sample1}"
 }
+
+variable "plain" {
+  default = "DevOps"
+}
+
+variable "List" {
+  default = ["AWS","DevOps","Data Science"]
+}
+
+variable "Map" {
+  default = {
+    DevOps = {
+      name = "DevOps"
+      time = "7AM"
+    }
+    AWS = {
+      name = "AWS"
+      time = "8AM"
+    }
+  }
+}
+
+output "o1" {
+  value = var.plain
+}
+output "o2" {
+  value = var.List
+}
+output "o3" {
+  value = var.Map.AWS
+}
