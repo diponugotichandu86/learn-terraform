b3722920d78f73577a8d4a648374320cb52ec6de
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-20 21:55:08 +0530

    roboshop project - Terraform

---
+++ 07-provisioners/main.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "example" {
+  owners = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0b7522441dfae1831"]
+  tags = {
+    Name = "frontend"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/diponugotichandu86/roboshop-ansible main.yml -e env=dev -e role_name=frontend"
+    ]
+  }
+}
+
