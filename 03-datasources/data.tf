46953719002375e219a5b758a8ee20fa3794b1fd
CHANDRASEKHAR <diponugotichandu@gmail.com>
2023-10-20 18:44:57 +0530

    roboshop project - Terraform

--- 03-datasources/data.tf
+++ 03-datasources/data.tf
@@ -1,17 +1,3 @@
-data "aws_ec2_spot_price" "example" {
-  instance_type     = "t3.medium"
-  availability_zone = "us-east-1"
-
-  filter {
-    name   = "product-description"
-    values = ["Linux/UNIX"]
-  }
-}
-
-#output "price" {
- # value = data.aws_ec2_spot_price.example.id
-#}
-
 data "aws_security_group" "selected" {
   name = "allow-all"
 }
